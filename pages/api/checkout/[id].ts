044b42bfa1350ddf1b306715cb25ae89e40db029
shayana-patel <shayana_patel@hotmail.com>
2022-02-24 11:26:29 +1300

    remove expand: [payment_intent] from [id].ts file

--- pages/api/checkout/[id].ts
+++ pages/api/checkout/[id].ts
@@ -7,6 +7,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {id} = req.query;
-  const session =  await stripe.checkout.sessions.retrieve(id as string, {expand: ['payment_intent']})
+  const session =  await stripe.checkout.sessions.retrieve(id as string)
   res.status(200).json({ session })
 }
